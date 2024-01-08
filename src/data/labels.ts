fc90dcef7cde0531c88b69ef89cd934026790073
Jowita Gogol <jmg88@bath.ac.uk>
2024-01-08 14:04:35 +0000

    Added data for labels in NavCardList

---
+++ src/data/labels.ts
@@ -0,0 +1,22 @@
+export const Labels = [
+  {
+    label: "New Appointments",
+    buttonLabel: "Go",
+  },
+  {
+    label: "Staff Details",
+    buttonLabel: "Go",
+  },
+  {
+    label: "Client Details",
+    buttonLabel: "Go",
+  },
+  {
+    label: "Resources",
+    buttonLabel: "Go",
+  },
+  {
+    label: "Settings",
+    buttonLabel: "Go",
+  },
+];
