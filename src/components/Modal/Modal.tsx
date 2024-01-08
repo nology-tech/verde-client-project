1d32e109883461cddf85ff48f2bf01089b9c5fc5
J M Adis <jessicaadis7@gmail.com>
2024-01-08 14:15:04 +0000

    testing Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,3 +1,19 @@
 import "./Modal.scss"
+import Button from "../Button/Button"
 
+const Modal = ({}) => {
+const handleClick = () => {
+    console.log("clicked")
+}
 
+    return (
+        <section>
+        <div>Hello World!</div>
+        <div>
+           <Button label="booking-confirmation" variant="yellow" onClick={handleClick}></Button> 
+        </div>
+        </section>
+    )
+}
+
+export default Modal
