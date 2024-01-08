292c02afd3534cb2ec6f6367ae3833a2d9610044
nrmistry <nilum@outlook.com>
2024-01-08 15:04:14 +0000

    added button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,25 @@
 import logo from "../../../public/images/logo.png";
+import Button from "../Button/Button";
 import "./Header.scss";
 
 type HeaderProps = {
   title: string;
   variant: "light" | "dark";
+  onClick: () => void;
+  buttonOption: boolean;
+
 };
 
-const Header = ({ title, variant }: HeaderProps) => {
+const Header = ({ title, variant, onClick, buttonOption }: HeaderProps) => {
   return (
     <div className={`header header--${variant}`}>
       <img src={logo} className="header__logo" alt="logo" />
       <h1 className="header__title">{title}</h1>
+      {}
+      <div className="header__button">
+      <Button label="Book" variant="yellow"  onClick={onClick}/>
+
+      </div>
     </div>
   );
 };
