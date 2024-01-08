f4c088ab1d5d18baa19926ad7b72941ccc78a0c3
nrmistry <nilum@outlook.com>
2024-01-08 10:55:34 +0000

    header content

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,1 +1,16 @@
-<h1> hello </h1>
+import logo from "../../../public/images/logo.png";
+
+type HeaderProps = {
+    headerTitle: string; 
+}
+
+const Header = ({headerTitle} : HeaderProps) => {
+    return (
+        <div className="header">
+        <img src={logo} className="header__logo" alt="logo"/>
+        <h1 className="header__title">{headerTitle}</h1>
+        </div>
+    )
+}
+
+export default Header;
