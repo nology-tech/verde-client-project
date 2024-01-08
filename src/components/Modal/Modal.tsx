bcf7aee9194bf9dfe8b7b529170bc4b88a67c159
J M Adis <jessicaadis7@gmail.com>
2024-01-08 14:43:49 +0000

    Have added modalprops to  modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,16 +1,27 @@
 import "./Modal.scss"
 import Button from "../Button/Button"
+import { Link } from "react-router-dom"
 
-const Modal = ({}) => {
+type ModalProps = {
+    title: string;
+    variant: "yellow" | "red";
+    label: string; 
+    onClick: () => void;
+    path: string; 
+}
+
+const Modal = ({title, variant, label, onClick, path} : ModalProps) => {
 const handleClick = () => {
     console.log("clicked")
 }
-
     return (
         <section>
-        <div>Hello World!</div>
-        <div>
-           <Button label="booking-confirmation" variant="yellow" onClick={handleClick}></Button> 
+        <h2>{title}</h2>
+        <div> 
+            <Link to = {path}>
+                <Button label={label} variant={variant} onClick={onClick}></Button> 
+                
+            </Link>
         </div>
         </section>
     )
