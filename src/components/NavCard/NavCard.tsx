1ff4f700c7c9d0e9e20b3fac86d6d4d87b1aa518
Jowita Gogol <jmg88@bath.ac.uk>
2024-01-08 12:27:35 +0000

    Created NavCard and NavCardList components

---
+++ src/components/NavCard/NavCard.tsx
@@ -0,0 +1,13 @@
+import "./NavCard.scss";
+
+// type NavCardProps = {
+//     label:
+// };
+
+const NavCard = () => {
+  return (
+    <p>Test</p>
+  );
+};
+
+export default NavCard;
---
+++ src/components/NavCardList/NavCardList.tsx
@@ -0,0 +1,13 @@
+import "./NavCardList.scss";
+
+// type NavCardProps = {
+//     label:
+// };
+
+const NavCard = () => {
+  return (
+    <p>Test</p>
+  );
+};
+
+export default NavCard;
