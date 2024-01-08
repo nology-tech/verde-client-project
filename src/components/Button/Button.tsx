84aa6c17213dec091e2948bb30414c75638551d9
Katarzyna Kozlowska <Kat.w.kozlowska@gmail.com>
2024-01-08 11:17:44 +0000

    updated styles on buttons and added on click functioanlity

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,12 +2,17 @@ import "./Button.scss";
 
 type ButtonProps = {
   label: string;
-  variant: "yellow" | "grey" | "red";
+  variant: "yellow" | "grey" | "red" | "purple";
+  onClick: () => void;
 };
 
-const Button = ({ label, variant }: ButtonProps) => {
+const Button = ({ label, variant, onClick }: ButtonProps) => {
   const className = `button button--${variant}`;
-  return <button className={className}>{label}</button>;
+  return (
+    <button className={className} onClick={onClick}>
+      {label}
+    </button>
+  );
 };
 
 export default Button;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,3 @@
-import Button from "../../components/Button/Button";
 import Layout from "../../components/Layout/Layout";
 
 const Home = () => {
@@ -8,9 +7,6 @@ const Home = () => {
       <h2>Section Heading</h2>
       <h3>Panel Heading</h3>
       <h4>Ollie made this change</h4>
-      <Button label={"Click me with longer text"} variant={"yellow"}/>
-      <Button label={"Click me"} variant={"grey"}/>
-      <Button label={"Click me"} variant={"red"}/>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
         dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
