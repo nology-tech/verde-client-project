83b261928de7e1c9c82422d9873fb306abf72469
sachie5 <sachamccormack@gmail.com>
2024-01-08 10:58:03 +0000

    created list and added icons to navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,1 +1,22 @@
 import "./NavBar.scss";
+
+type NavBarProps = {
+
+};
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><img src="" alt="Home Button"/>Home</li>
+                <li><img src="" alt="Book Appointment"/>Book Appointment</li>
+                <li><img src="" alt="Staff"/>Staff</li>
+                <li><img src="" alt="Clients"/>Clients</li>
+                <hr/>
+                <li><img src="" alt="Settings"/>Settings</li>
+            </ul>
+        </nav>
+    )
+};
+
+export default NavBar;
