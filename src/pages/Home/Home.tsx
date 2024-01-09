d92fa6320c1a9c54008b3faf3cddf08bf5b76746
AISimonetta <info@simonettaalejandra.com>
2024-01-09 16:20:11 +0000

    mobile view home section  styled

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,9 @@ import Layout from "../../components/Layout/Layout";
 import NavBar from "../../components/NavBar/NavBar";
 import Header from "../../components/Header/Header";
 import Button from  "../../components/Button/Button";
-
+import NavCardList from "../../components/NavCardList/NavCardList"
+import{NavCardLabels} from "../../data/NavCardLabels"
+import './Home.scss'
 
 const Home = () => {
 
@@ -13,8 +15,11 @@ const Home = () => {
 <Layout>
       <NavBar variant={"light"}/>
       <main className="main">
-      <Header title="Home" variant="light" onClick={onClick} buttonOption={false} buttonLabel="Home" buttonVariant="purple" />
-      <Button size="large" label="Log Out" variant="yellow" onClick={onClick} path=""/>
+          <Header title="Home" variant="light" onClick={onClick} buttonOption={false} buttonLabel="Home" buttonVariant="purple" />
+          <div className="main__container">
+            <NavCardList labels={NavCardLabels} variant={"light"} />
+            <Button size="large" label="Log Out" variant="yellow" onClick={onClick} path=""/>
+          </div>
       </main>
     </Layout>
   );
