3559833b909c3d7e63eabd2f57401e3648108b2d
Kumani K <kumanikidd@gmail.com>
2024-01-09 14:21:33 +0000

    Modal now accepts button array

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,20 +1,12 @@
 import "./Modal.scss"
-import Button from "../Button/Button"
 
 /* Modal takes button object array as prop. This is to allow for multiple buttons on the modal.   */
 type ModalProps = {
     title: string;
-    buttons: ButtonProps[];
+    buttons: React.ReactNode[];
     variant: "light" | "dark"
 }
 
-type ButtonProps = {
-    label: string;
-    buttonVariant: "yellow" | "grey" | "red" | "purple" | "red-alt";
-    onClick?: () => void
-    path?: string; 
-    size?: 'large' | 'small'
-}
 
 const Modal = ({title, buttons, variant} : ModalProps) => {
 
@@ -22,15 +14,7 @@ const Modal = ({title, buttons, variant} : ModalProps) => {
         <div className={`modal modal--${variant}`} >
             <h2 className="modal__title" >{title}</h2>
             <div className="modal__buttons"> 
-                {buttons.map((button) => (
-                        <Button
-                            label={button.label}
-                            variant={button.buttonVariant}
-                            onClick={button.onClick}
-                            path={button.path}
-                            size={button.size}
-                        />
-                ))}
+                {buttons.map((button) => (button))}
             </div>
         </div>
     );
