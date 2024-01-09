3584478116678b09bbd1e7612d27ce2302b8f176
AISimonetta <info@simonettaalejandra.com>
2024-01-09 09:52:30 +0000

    props added for lightmode darkmode screen view

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,16 @@
 import "./Footer.scss";
 import { Link } from "react-router-dom";
 
-const Footer = () => {
+type FooterProps = {
+   variant : "lightMode" | "darkMode"
+};
+
+const Footer = ( { variant } : FooterProps ) => {
+    const className = `footer footer--${variant}`;
 
     return (
-        <footer className="footer">
-           <Link to="/"><button className="footer__button"><img src="/src/assets/images/homeFooter-icon.svg" alt="home-button" className="footer__button--image"/></button></Link>
+        <footer className= {className}>
+            <Link to="/"><button className="footer__button"><img src="/src/assets/images/homeFooter-icon.svg" alt="home-button" className="footer__button--image"/></button></Link>
         </footer>
     )
 };
