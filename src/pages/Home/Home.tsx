25d549e3f4e7f798fa39d556f23ab8ee5a2c54b2
sachie5 <sachamccormack@gmail.com>
2024-01-09 15:10:16 +0000

    edited styles of layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,22 @@
+import Button from "../../components/Button/Button";
+import Header from "../../components/Header/Header";
 import Layout from "../../components/Layout/Layout";
+import NavBar from "../../components/NavBar/NavBar";
+import "./Home.scss";
 
 const Home = () => {
+
+  const onClick = () => {
+
+  };
+
   return (
     <Layout>
-      <h1>Page Heading</h1>
-      <h2>Section Heading</h2>
-      <h3>Panel Heading</h3>
-      <h4>Ollie made this change</h4>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
-        dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
-        maxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
-        necessitatibus.
-      </p>
+      <NavBar />
+      <main className="main">
+      <Header title="Home" variant="light" onClick={onClick} buttonOption={false} buttonLabel="Home" buttonVariant="purple" />
+      <Button size="large" label="Log Out" variant="yellow" onClick={onClick} path=""/>
+      </main>
     </Layout>
   );
 };
