ed71490f5f066b5f2aa43d0aaa1c9b861f56e0f6
AISimonetta <info@simonettaalejandra.com>
2024-01-09 15:19:26 +0000

    compenents added to home, some styles added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,21 @@
 import Layout from "../../components/Layout/Layout";
+import NavBar from "../../components/NavBar/NavBar";
+import Header from "../../components/Header/Header";
+import Button from  "../../components/Button/Button";
 
 
 const Home = () => {
+
+  const onClick = () => {
+
+  }
   return (
-    <Layout>
-      <h1>Page Heading</h1>
-      <h2>Section Heading</h2>
-      <h3>Panel Heading</h3>
-      <h4>Ollie made this change</h4>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
-        dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
-        maxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
-        necessitatibus.
-      </p>
+<Layout>
+      <NavBar variant={"light"}/>
+      <main className="main">
+      <Header title="Home" variant="light" onClick={onClick} buttonOption={false} buttonLabel="Home" buttonVariant="purple" />
+      <Button size="large" label="Log Out" variant="yellow" onClick={onClick} path=""/>
+      </main>
     </Layout>
   );
 };
