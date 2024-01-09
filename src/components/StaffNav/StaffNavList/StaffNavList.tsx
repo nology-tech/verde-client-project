0ad7d1fe156907502614403e69e5832cd6b9c602
Jowita Gogol <jmg88@bath.ac.uk>
2024-01-09 10:28:59 +0000

    Added data file for staff

--- src/components/StaffNav/StaffNavList/StaffNavList.tsx
+++ src/components/StaffNav/StaffNavList/StaffNavList.tsx
@@ -1,79 +1,21 @@
-
 import { Link } from "react-router-dom";
+import { Staff } from "../data";
 
-const StaffList = [{
-    id:1,
-    name:"Staff1",
-    start:"01/01/2022",
-    end: "02/01/2022",
-    manager: "Manager 1",
-    role:"role",
-    description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-},
-{
-    id:2,
-    name:"Staff2",
-    start:"01/01/2022",
-    end: "02/01/2022",
-    manager: "Manager 1",
-    role:"role",
-    description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-},
-{
-    id:3,
-    name:"Staff3",
-    start:"01/01/2022",
-    end: "02/01/2022",
-    manager: "Manager 1",
-    role:"role",
-    description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-},
-{
-    id:4,
-    name:"Staff4",
-    start:"01/01/2022",
-    end: "02/01/2022",
-    manager: "Manager 1",
-    role:"role",
-    description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-},
-{
-    id:5,
-    name:"Staff5",
-    start:"01/01/2022",
-    end: "02/01/2022",
-    manager: "Manager 1",
-    role:"role",
-    description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit."
-},
-]
+type StaffNavListProps = {
+  staff: Staff;
+};
 
-type Staff = [{
-    id:number,
-    name:string,
-    start:string,
-    end: string,
-    manager: string,
-    role:string,
-    description:string
+const StaffNavList = ({ staff }: StaffNavListProps) => {
+  return (
+    <div className="staffnavlist">
+      {staff.map((employee) => (
+        <Link to="/" key={employee.id}>
+            
+        </Link>
+      ))}{" "}
+      {/* this is to be updatedd with name and surname? */}
+    </div>
+  );
+};
 
-}]
-
-type StaffNAvListProps = {
-  staff: Staff[];
-
-}
-
-
-const StaffNavList = ({staff}:StaffNAvListProps) =>{
-    return(
-        <div className="stafnav-list">
-        {staff.map((name)=> (
-            <Link to="/" key={name.id}
-         ))} {/* this is to be updatedd with name and surname? */}
-
-        </div>
-    );
-}
-
-export default StaffNavList
+export default StaffNavList;
---
+++ src/components/StaffNav/data.ts
@@ -0,0 +1,59 @@
+export type Staff = [
+  {
+    id: number,
+    name: string,
+    start: string,
+    courseEnd: string,
+    manager: string,
+    role: string,
+    description: string,
+  }
+];
+
+export const StaffList = [
+  {
+    id: 1,
+    name: "Staff1",
+    start: "01/01/2022",
+    courseEnd: "02/01/2022",
+    manager: "Manager 1",
+    role: "role",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 2,
+    name: "Staff2",
+    start: "01/01/2022",
+    courseEnd: "02/01/2022",
+    manager: "Manager 1",
+    role: "role",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 3,
+    name: "Staff3",
+    start: "01/01/2022",
+    courseEnd: "02/01/2022",
+    manager: "Manager 1",
+    role: "role",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 4,
+    name: "Staff4",
+    start: "01/01/2022",
+    courseEnd: "02/01/2022",
+    manager: "Manager 1",
+    role: "role",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 5,
+    name: "Staff5",
+    start: "01/01/2022",
+    courseEnd: "02/01/2022",
+    manager: "Manager 1",
+    role: "role",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+];
