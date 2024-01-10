e6b32dc7615aa806d34499bbd02b421d968fcf96
J M Adis <jessicaadis7@gmail.com>
2024-01-10 10:03:03 +0000

    staffbookinglist wip

--- src/components/StaffBookingList/StaffBookingList.tsx
+++ src/components/StaffBookingList/StaffBookingList.tsx
@@ -1,3 +1,15 @@
 import './StaffBookingList.scss'
+import StaffBookingCard from "../StaffBookingCard/StaffBookingCard"
+import React from 'react'
 
+const StaffBookingList = () => {
+    return (
+        <div className='booking-list'>
+            <p>Client Name</p>
+            <StaffBookingCard clientName='Joe Bloggs' email="joe.bloggs@gmail.com" mobileNumber='0987654321' bookingDate='1/1/2024' bookingTime='1:30' path="#" />
+        </div>
+    )
 
+}
+
+export default StaffBookingList
