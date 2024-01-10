a88d4c8d6823ec679a34b8d4029ff87af5161894
J M Adis <jessicaadis7@gmail.com>
2024-01-10 10:22:10 +0000

    created StaffBookingsList and StaffBookingsListType

--- src/components/StaffBookingList/StaffBookingList.tsx
+++ src/components/StaffBookingList/StaffBookingList.tsx
@@ -1,11 +1,22 @@
 import './StaffBookingList.scss'
 import StaffBookingCard from "../StaffBookingCard/StaffBookingCard"
+import { StaffBookings } from "../../types/StaffBookingsTypes"
+import { StaffBookingsList } from "../../data/StaffBookingsList"
 import React from 'react'
+
+type StaffBookingListProps = {
+    bookings: StaffBookings[];
+
+} 
 
 const StaffBookingList = () => {
     return (
         <div className='booking-list'>
             <p>Client Name</p>
+            <p>Email Address</p>
+            <p>Mobile Number</p>
+            <p>Booking Date</p>
+            <p>Booking Time</p>
             <StaffBookingCard clientName='Joe Bloggs' email="joe.bloggs@gmail.com" mobileNumber='0987654321' bookingDate='1/1/2024' bookingTime='1:30' path="#" />
         </div>
     )
---
+++ src/data/StaffBookingsList.ts
@@ -0,0 +1,51 @@
+export const StaffBookingsList = [
+{
+id: 1,
+clientName: "Joe Bloggs", 
+emailAddress: 'joe.bloggs@getDefaultNormalizer.com',
+mobNumber: "0987654321",
+bookingDate: "1/1/2024",
+bookingTime: "1:30",
+path: "#",
+},
+
+{
+id: 2,
+clientName: "Joe Bloggs", 
+emailAddress: 'joe.bloggs@getDefaultNormalizer.com',
+mobNumber: "0987654321",
+bookingDate: "1/1/2024",
+bookingTime: "1:30",
+path: "#",
+},
+
+{
+id: 3,
+clientName: "Joe Bloggs", 
+emailAddress: 'joe.bloggs@getDefaultNormalizer.com',
+mobNumber: "0987654321",
+bookingDate: "1/1/2024",
+bookingTime: "1:30",
+path: "#",
+},
+
+{
+id: 4,
+clientName: "Joe Bloggs", 
+emailAddress: 'joe.bloggs@getDefaultNormalizer.com',
+mobNumber: "0987654321",
+bookingDate: "1/1/2024",
+bookingTime: "1:30",
+path: "#",
+},
+
+{
+id: 5,
+clientName: "Joe Bloggs", 
+emailAddress: 'joe.bloggs@getDefaultNormalizer.com',
+mobNumber: "0987654321",
+bookingDate: "1/1/2024",
+bookingTime: "1:30",
+path: "#",
+},
+]
