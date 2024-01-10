3d0b5627f5b66cb80bdab81c2ea2d1455d3a7d90
Katarzyna Kozlowska <Kat.w.kozlowska@gmail.com>
2024-01-10 12:42:32 +0000

    routed login button to home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,30 @@
 import Layout from "../../components/Layout/Layout";
 import NavBar from "../../components/NavBar/NavBar";
 import Header from "../../components/Header/Header";
-import Button from  "../../components/Button/Button";
-import NavCardList from "../../components/NavCardList/NavCardList"
-import{NavCardLabels} from "../../data/NavCardLabels"
-import './Home.scss'
+import Button from "../../components/Button/Button";
+import NavCardList from "../../components/NavCardList/NavCardList";
+import { NavCardLabels } from "../../data/NavCardLabels";
+import "./Home.scss";
 type HomeProps = {
   variant: "light" | "dark";
 };
 
 const Home = ({ variant }: HomeProps) => {
-return (
-<Layout>
-      <NavBar variant={variant}/>
-      <main  className={`main main--${variant}`}>
-          <Header title="Home" variant={variant} buttonOption={false} buttonLabel="Home" buttonVariant="purple" />
-          <div className="main__container">
-            <NavCardList labels={NavCardLabels} variant={variant} />
-            <Button size="large" label="Log Out" variant="yellow"  path="/login"/>
-          </div>
+  return (
+    <Layout>
+      <NavBar variant={variant} />
+      <main className={`main main--${variant}`}>
+        <Header
+          title="Home"
+          variant={variant}
+          buttonOption={false}
+          buttonLabel="Home"
+          buttonVariant="purple"
+        />
+        <div className="main__container">
+          <NavCardList labels={NavCardLabels} variant={variant} />
+          <Button size="large" label="Log Out" variant="yellow" path="/login" />
+        </div>
       </main>
     </Layout>
   );
