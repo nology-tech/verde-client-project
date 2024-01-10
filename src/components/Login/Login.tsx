31d5b718965f1b22e2f7058fabdb33394a83c7cd
Jowita Gogol <jmg88@bath.ac.uk>
2024-01-10 10:04:41 +0000

    Started media query for laptop view

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -8,45 +8,47 @@ const Login = () => {
 
   return (
     <div className="login">
-      <img src={logo} alt="logo" className="login__logo" />
-      <div className="login__container">
-        <h2 className="login__welcome">Welcome Back</h2>
-        <p className="login__message">
-          Feugiat magna viverra pellentesque habitant. Libero id amet nisl,
-          tellus cras eleifend et in.
-        </p>
-        <form className="login__form">
-          <div className="login__form-container">
-            <label htmlFor="email" className="login__form--label">
-              Email Address
-            </label>
-            <input
-              type="email"
-              id="email"
-              required
-              className="login__form--input"
-            />
-          </div>
-          <div className="login__form-container">
-            <label htmlFor="password" className="login__form--label">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              required
-              className="login__form--input"
-            />
-          </div>
-          <div className="login__form--button">
-            <Button
-              label={"Login"}
-              variant={"yellow"}
-              size={"extra-large"}
-              onClick={() => void {}}
-            />
-          </div>
-        </form>
+      <div className="login__page">
+        <img src={logo} alt="logo" className="login__logo" />
+        <div className="login__container">
+          <h2 className="login__welcome">Welcome Back</h2>
+          <p className="login__message">
+            Feugiat magna viverra pellentesque habitant. Libero id amet nisl,
+            tellus cras eleifend et in.
+          </p>
+          <form className="login__form">
+            <div className="login__form-container">
+              <label htmlFor="email" className="login__form--label">
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                required
+                className="login__form--input"
+              />
+            </div>
+            <div className="login__form-container">
+              <label htmlFor="password" className="login__form--label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                required
+                className="login__form--input"
+              />
+            </div>
+            <div className="login__form--button">
+              <Button
+                label={"Login"}
+                variant={"yellow"}
+                size={"extra-large"}
+                onClick={() => void {}}
+              />
+            </div>
+          </form>
+        </div>
       </div>
       <img className="login__picture" src={login} />
     </div>
