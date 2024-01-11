caf03e24d04711552e0c6e4acaebb5a167409c74
Matthew <12hughesm@gmail.com>
2024-01-11 12:05:05 +0000

    Removed testing code

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,8 +5,6 @@ import Button from "../../components/Button/Button";
 import NavCardList from "../../components/NavCardList/NavCardList";
 import { NavCardLabels } from "../../data/NavCardLabels";
 import "./Home.scss";
-import StaffBookingList from "../../components/StaffBookingList/StaffBookingList";
-import { StaffBookingsList } from "../../data/StaffBookingsList";
 type HomeProps = {
   variant: "light" | "dark";
 };
@@ -27,7 +25,6 @@ const Home = ({ variant }: HomeProps) => {
           <NavCardList labels={NavCardLabels} variant={variant} />
           <Button size="large" label="Log Out" variant="yellow" path="/login" />
         </div>
-      <StaffBookingList bookings={StaffBookingsList} variant="dark"/>
       </main>
     </Layout>
   );
