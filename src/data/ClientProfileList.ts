ea1537adf32db48e0ad997f34844146ca063b372
J M Adis <jessicaadis7@gmail.com>
2024-01-11 14:07:52 +0000

    created mock data for client profile cards

---
+++ src/data/ClientProfileList.ts
@@ -0,0 +1,55 @@
+export const ClientProfileList = [
+
+    {
+        clientName: "John Doe",
+        dob: "24/03/1997", 
+        emailAddress: "john.doe@gmail.com", 
+        mobileNumber: "12345678910",
+        appointmentDate: "01/02/2024", 
+        appointmentTime: "12:30pm",
+        staffMember: "Staff 01",
+    },
+    {
+        clientName: "Bob Bob",
+        dob: "26/03/1999",
+        emailAddress: "bob.bob@gmail.com",
+        mobileNumber: "01928273847",
+        appointmentDate: "01/01/2022",
+        appointmentTime: "13.00pm",
+        staffMember: "Staff 03",
+      },
+      {
+        clientName: "Lala land",
+        dob: "13/05/1992",
+        emailAddress: "lala.land@gmail.com",
+        mobileNumber: "01928273898",
+        appointmentDate: "05/01/2022",
+        appointmentTime: "17.00pm",
+        staffMember: "Staff 02",
+      },
+      {
+        clientName: "Teheee Pee",
+        dob: "19/09/1990",
+        emailAddress: "tehee.pee@gmail.com",
+        mobileNumber: "01928272777",
+        appointmentDate: "07/02/2022",
+        appointmentTime: "18.00pm",
+        staffMember: "Staff 01",
+      },
+      {
+        clientName: "Hells Mells",
+        dob: "04/02/1996",
+        emailAddress: "hells.mells@gmail.com",
+        mobileNumber: "01928272888",
+        appointmentDate: "07/02/2022",
+        appointmentTime: "14.00pm",
+        staffMember: "Staff 01",
+      },
+]
+
+
+
+
+
+
+
