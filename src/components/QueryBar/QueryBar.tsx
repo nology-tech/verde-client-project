107908a325a38806b8959adeccc07578b05139b2
Kumani K <kumanikidd@gmail.com>
2024-01-11 16:24:44 +0000

    updated props in Querybar and hidden on mobile

--- src/components/QueryBar/QueryBar.tsx
+++ src/components/QueryBar/QueryBar.tsx
@@ -3,31 +3,39 @@ import "./QueryBar.scss";
 
 type QueryBarProps = {
   label: string;
+  searchTerm: string;
   hasFilter: boolean;
-  onClick: () => void;
   handleInput: () => void;
+  columnViewClick: () => void;
+  gridViewClick: () => void;
+  sortClick: () => void;
+  filterClick: () => void;
 };
 
 const QueryBar = ({
   label,
+  searchTerm,
   hasFilter,
-  onClick,
+  sortClick,
+  filterClick,
   handleInput,
+  columnViewClick,
+  gridViewClick
 }: QueryBarProps) => {
   return (
     <div className="querybar">
       <h3 className="querybar__title">{label}</h3>
       <div className="querybar__views">
-        <img className="querybar__views--column" src="images/column-view.png" />
-        <img className="querybar__views--grid" src="images/grid-view.png" />
+        <img className="querybar__views--column" src="images/column-view.png" onClick={columnViewClick}/>
+        <img className="querybar__views--grid" src="images/grid-view.png" onClick={gridViewClick} />
       </div>
-      <SearchBox />
-      <div className="querybar__sort">
+      <SearchBox handleInput={handleInput} searchTerm={searchTerm}/>
+      <div className="querybar__sort" onClick={sortClick}>
         <img className="querybar__sort--img" src="images/sort.png" />
         <p className="querybar__sort--text">Sort</p>
       </div>
       {hasFilter && (
-        <div className="querybar__filter">
+        <div className="querybar__filter" onClick={filterClick}>
           <img className="querybar__filter--img" src="images/filter.png" />
           <p className="querybar__filter--text">Filter</p>
         </div>
