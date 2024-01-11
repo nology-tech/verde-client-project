0b19e65554e2a967c98314c75cfc34fe0c79cd8c
Kumani K <kumanikidd@gmail.com>
2024-01-11 12:39:24 +0000

    added setup for querybar and search box

---
+++ src/components/QueryBar/QueryBar.tsx
@@ -0,0 +1,33 @@
+import SearchBox from '../SearchBox/SearchBox';
+import './QueryBar.scss'
+
+type QueryBarProps = {
+    label: string;
+    hasFilter: boolean;
+    onClick: () => void;
+    handleInput: () => void;
+}
+
+const QueryBar = ({label, hasFilter, onClick, handleInput}: QueryBarProps) => {
+    return (
+        <div className='querybar'>
+            <h3 className='querybar__title'>{label}</h3>
+            <div className='querybar__views'>
+                <img className='querybar__views--column' src='images/column-view.png' />
+                <img className='querybar__views--grid' src='images/grid-view.png' />
+            </div>
+            <SearchBox />
+            <div className='querybar__sort'>
+                <img className='querybar__sort--img' src='images/sort.png' />
+                <p>Sort</p>
+            </div>
+            <div className='querybar__filter'>
+                <img className='querybar__filter--img' src='images/filter.png' />
+                <p>Filter</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default QueryBar
