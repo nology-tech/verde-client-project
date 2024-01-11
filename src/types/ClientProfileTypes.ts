38688b063292e6b7c32cc23a50594770b370dcb6
J M Adis <jessicaadis7@gmail.com>
2024-01-11 14:08:13 +0000

    created clientinfo type for data

---
+++ src/types/ClientProfileTypes.ts
@@ -0,0 +1,9 @@
+export type ClientInfo = {
+    clientName: string;
+    dob: string; 
+    emailAddress: string; 
+    mobileNumber: string; 
+    appointmentDate: string; 
+    appointmentTime: string;
+    staffMember: string;
+}
