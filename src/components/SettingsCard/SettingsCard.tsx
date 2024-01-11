1afa57b7f3c76c75ebd19c6b4900b20c71d4f92e
Jowita Gogol <jmg88@bath.ac.uk>
2024-01-11 13:12:59 +0000

    Updated styles for SettingCard - mobile view

--- src/components/SettingsCard/SettingsCard.tsx
+++ src/components/SettingsCard/SettingsCard.tsx
@@ -16,49 +16,54 @@ const SettingsCard = () => {
     <div className="settings-card">
       <div className="settings-card__section">
         <h2 className="settings-card__section--title">Light/Dark Mode:</h2>
-        <img
-          className="setting-card__icon setting-card__icon--sun"
-          src={sun}
-          alt="Sun Icon"
-        ></img>
-        <ToggleSwitch onToggle={handleToggleChange} />
-        <img
-          className="setting-card__icon setting-card__icon--moon"
-          src={moon}
-          alt="Moon Icon"
-        ></img>
+        <div className="settings-card__container-mode">
+          <img
+            className="settings-card__icon settings-card__icon--sun"
+            src={sun}
+            alt="Sun Icon"
+          ></img>
+          <ToggleSwitch onToggle={handleToggleChange} />
+          <img
+            className="settings-card__icon settings-card__icon--moon"
+            src={moon}
+            alt="Moon Icon"
+          ></img>
+        </div>
       </div>
       <div className="settings-card__section">
         <h2 className="settings-card__section--title">Select Theme:</h2>
-        <div className="settings-card__radio">
-          <label>
-            <input
-              className="settings-card__radio--label"
-              type="radio"
-              value="enabled"
-              name="theme"
-            />
-            Modern
-          </label>
+        <div className="settings-card__container-theme">
+          <div className="settings-card__radio">
+            <label className="settings-card__radio-container">
+              <p className="settings-card__option">Modern</p>
+              <input
+                className="settings-card__radio--label"
+                type="radio"
+                value="enabled"
+                name="theme"
+              />
+            </label>
+          </div>
+          <div className="settings-card__radio">
+            <label className="settings-card__radio-container">
+              <p className="settings-card__option">Tech</p>
+              <input
+                className="settings-card__radio--label"
+                type="radio"
+                value="disabled"
+                name="theme"
+              />
+            </label>
+          </div>
         </div>
-        <div className="settings-card__radio">
-          <label>
-            <input
-              className="settings-card__radio--label"
-              type="radio"
-              value="disabled"
-              name="theme"
-            />
-            Tech
-          </label>
+        <div className="settings-card__button">
+          <Button
+            label={"Apply"}
+            variant={"yellow"}
+            size={"small"}
+            onClick={() => void {}}
+          />
         </div>
-        <Button
-          label={"Apply"}
-          variant={"yellow"}
-          size={"small"}
-          path="/home"
-          onClick={() => void {}}
-        />
       </div>
     </div>
   );
