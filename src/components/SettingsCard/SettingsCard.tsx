e4dab9ac03740730f387c57437fa4327dfedf7c9
nrmistry <nilum@outlook.com>
2024-01-11 09:40:53 +0000

    styles

--- src/components/SettingsCard/SettingsCard.tsx
+++ src/components/SettingsCard/SettingsCard.tsx
@@ -1,26 +1,38 @@
 import "./SettingsCard.scss";
+import {useState} from "react";
+
 
 const SettingsCard = () => {
+
+    const [isDarkMode, setIsDarkMode] = useState(false);
+  
+    const handleToggleChange = () => {
+      setIsDarkMode(!isDarkMode);
+    }
+
     return (
       <div className="settings-card">
         <div className="settings-card__section">
-          <h2 className="settings-card__section-title">Light/Dark Mode:</h2>
+          <h2 className="settings-card__section--title">Light/Dark Mode:</h2>
           <label className="settings-card__toggle">
-            <input type="checkbox" className="settings-card__toggle--checkbox" />
+            <input type="checkbox" 
+            className="settings-card__toggle--checkbox"
+            checked={isDarkMode}
+            onChange={handleToggleChange}/>
             <span className="settings-card__toggle--slider"></span>
           </label>
         </div>
         <div className="settings-card__section">
-          <h2 className="settings-card__section-title">Select Theme:</h2>
+          <h2 className="settings-card__section--title">Select Theme:</h2>
           <div className="settings-card__radio">
             <label>
-              <input type="radio" value="enabled" name="theme"/>
+              <input className="settings-card__radio--label" type="radio" value="enabled" name="theme"/>
               Modern
             </label>
           </div>
           <div className="settings-card__radio">
             <label>
-              <input type="radio" value="disabled" name="theme" />
+              <input className="settings-card__radio--label" type="radio" value="disabled" name="theme" />
               Tech
             </label>
           </div>
