a47a1ace79b1607ffe7b2d98ebf3b9394cc5352b
sachie5 <sachamccormack@gmail.com>
2024-01-12 15:47:00 +0000

    filtered bookings by client name

--- src/pages/Staff/Staff.tsx
+++ src/pages/Staff/Staff.tsx
@@ -11,47 +11,53 @@ import { StaffBookingsList } from "../../data/StaffBookingsList";
 import Justify from "../../assets/images/bi-justify.png";
 import Justify_alt from "../../assets/images/bi-grid-3x3-gap-fill.png";
 import Sort from "../../assets/images/sort.png";
-import Search from "../../assets/images/search.png"
+import Search from "../../assets/images/search.png";
 import { ChangeEvent, useState } from "react";
 
 type StaffProps = {
   variant: "light" | "dark";
 };
 
- 
-  
-
 const Staff = ({ variant }: StaffProps) => {
-  const [dropDown, setDropDown] = useState<string>("Staff 1")
+  const [dropDown, setDropDown] = useState<string>("Staff 1");
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
-  const handleDropDown = (event:ChangeEvent<HTMLSelectElement>) => {
-    setDropDown(event.currentTarget.value)
-    console.log(dropDown)
-  }
-  
-  const filteredStaff = StaffList.filter(staff =>
-     staff.name === dropDown)
+  const handleDropDown = (event: ChangeEvent<HTMLSelectElement>) => {
+    setDropDown(event.currentTarget.value);
+    console.log(dropDown);
+  };
+
+  const handleSearchTerm = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.currentTarget.value);
+  };
+
+  const filteredStaff = StaffList.filter((staff) => staff.name === dropDown);
+
+  const filteredBookings = StaffBookingsList.filter((booking) => {
+    return booking.clientName.toLowerCase().includes(searchTerm);
+  });
 
   return (
-      <Layout>
-        <NavBar variant={variant} />
-        <main className={`main main--${variant}`}>
+    <Layout>
+      <NavBar variant={variant} />
+      <main className={`main main--${variant}`}>
         <Header
-            title={"Staff"}
-            variant={"light"}
-            buttonOption={false}
-            buttonLabel={""}
-            buttonVariant={"yellow"}
-            dropdownOption={true}
-            onChange={handleDropDown}
-          />
-        
-          <section className="profile--section">
-            <h1 className="profile--section__heading">Overview</h1>
-            {filteredStaff.map(staff => <StaffProfileCard
+          title={"Staff"}
+          variant={"light"}
+          buttonOption={false}
+          buttonLabel={""}
+          buttonVariant={"yellow"}
+          dropdownOption={true}
+          onChange={handleDropDown}
+        />
+
+        <section className="profile--section">
+          <h1 className="profile--section__heading">Overview</h1>
+          {filteredStaff.map((staff) => (
+            <StaffProfileCard
               staff={{
                 id: Number(`${staff.id}`),
-                name:`${staff.name}`,
+                name: `${staff.name}`,
                 start: `${staff.start}`,
                 courseEnd: `${staff.courseEnd}`,
                 manager: `${staff.manager}`,
@@ -59,27 +65,39 @@ const Staff = ({ variant }: StaffProps) => {
                 description: `${staff.description}`,
               }}
               variant={"light"}
-            />)}
-          </section>
-          <StaffNavList staff={StaffList} variant={"light"} />
-          <section className="booking">
-            <div className="booking__menu">
-              <h1 className="booking__staff">Bookings</h1>
-              <div className="display">
-              <button className="booking__menu--buttons"><img className="booking__menu--icons" src={Justify} /></button>
-              <button className="booking__menu--buttons"><img className="booking__menu--icons" src={Justify_alt} /></button>
-              </div>
-              <div className="search-bar">
-              <input className="booking__menu--input" type="text"></input>
-              <img className= "booking__menu--icon" src={Search}></img>
-              </div>
-              <button className="booking__menu--buttons"><img className="booking__menu--icons" src={Sort} /></button>
+            />
+          ))}
+        </section>
+        <StaffNavList staff={StaffList} variant={"light"} />
+        <section className="booking">
+          <div className="booking__menu">
+            <h1 className="booking__staff">Bookings</h1>
+            <div className="display">
+              <button className="booking__menu--buttons">
+                <img className="booking__menu--icons" src={Justify} />
+              </button>
+              <button className="booking__menu--buttons">
+                <img className="booking__menu--icons" src={Justify_alt} />
+              </button>
             </div>
-            <StaffBookingList bookings={StaffBookingsList} variant={"light"} /> 
-          </section>
-        </main>
-        <Footer variant={"light"} />
-      </Layout>
+            <div className="search-bar">
+              <input
+                className="booking__menu--input"
+                type="text"
+                value={searchTerm}
+                onChange={handleSearchTerm}
+              ></input>
+              <img className="booking__menu--icon" src={Search}></img>
+            </div>
+            <button className="booking__menu--buttons">
+              <img className="booking__menu--icons" src={Sort} />
+            </button>
+          </div>
+          <StaffBookingList bookings={filteredBookings} variant={"light"} />
+        </section>
+      </main>
+      <Footer variant={"light"} />
+    </Layout>
   );
 };
 
