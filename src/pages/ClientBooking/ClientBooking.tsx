d77aa148966be5d973bcb1065557b13eaf129e84
Chelsea Li <148104000+Chelsealily@users.noreply.github.com>
2024-01-12 12:50:43 +0000

    updates

--- src/pages/ClientBooking/ClientBooking.tsx
+++ src/pages/ClientBooking/ClientBooking.tsx
@@ -1,3 +1,4 @@
+import ClientProfile from "../../components/ClientProfile/ClientProfile"
 import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
 import Layout from "../../components/Layout/Layout"
@@ -21,12 +22,27 @@ const ClientBooking = ({variant}:ClientBookingProps) => {
         buttonVariant="yellow"
         dropdownOption={false}
       />
-      <div className="main__container">
-        <h3 className="main__container--subtitle"> Overview</h3>
-        
-      </div>
-    </main>
+        <div>
+       
+       </div>
+       <div className="main__container">
+        <h3 className="main__subtitle"> Overview</h3>
+        <ClientProfile clientinfo={{
+                      clientName: "Bob",
+                      role: "Manager",
+                      dob: "30/09/1997",
+                      emailAddress: "bob@live.com",
+                      mobileNumber: "9782748959",
+                      appointmentDate: "20/05/2022",
+                      appointmentTime: "14.00pm",
+                      staffMember: "Staff 03"
+                  }} variant={"light"}/>
+      <div>
     <Footer variant={variant}/> 
+    </div>
+    </div>
+    </main>
+    
   </Layout>
   )
 }
