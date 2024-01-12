c82cc8c1989669c678b86c45bb025f11020db5be
Jowita Gogol <jmg88@bath.ac.uk>
2024-01-12 12:29:42 +0000

    Updated styles for Settings page

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,39 +1,38 @@
 import "./Settings.scss";
-import Layout from "../../components/Layout/Layout"; 
+import Layout from "../../components/Layout/Layout";
 import NavBar from "../../components/NavBar/NavBar";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Button from "../../components/Button/Button";
+import SettingsCard from "../../components/SettingsCard/SettingsCard";
 
 type SettingsProps = {
-    variant: "light" | "dark";
-}
+  variant: "light" | "dark";
+};
 
-const Settings = ({variant}:SettingsProps) => {
-
-    return(
-
+const Settings = ({ variant }: SettingsProps) => {
+  return (
     <Layout>
-    <NavBar variant={variant} />
-    <main className={`main main--${variant}`}>
-      <Header
-        title="Settings"
-        variant={variant}
-        buttonOption={true}
-        buttonLabel="Log out"
-        buttonVariant="yellow"
-        dropdownOption={false}
-      />
-      <div className="settings-container">
-      <div className="main__container">
-        <Button label="Log Out" variant="red" path="/" />
-      </div>
-      </div>
-    </main>
-    <Footer variant={variant}/>
-  </Layout>
-    )
-
-}
+      <NavBar variant={variant} />
+      <main className={`settings settings--${variant}`}>
+        <Header
+          title="Settings"
+          variant={variant}
+          buttonOption={true}
+          buttonLabel="Log out"
+          buttonVariant="yellow"
+          dropdownOption={false}
+        />
+        <div className="settings__container">
+          <SettingsCard variant={variant} />
+          <div className="settings__card">
+            <Button label="Log Out" variant="red" path="/" />
+          </div>
+        </div>
+      </main>
+      <Footer variant={variant} />
+    </Layout>
+  );
+};
 
 export default Settings;
