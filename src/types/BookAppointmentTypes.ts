676a04051e1dcabf35a0cd8317adae89c724e19e
DeeqaJamalini <deeqa.j11@gmail.com>
2024-01-12 15:57:58 +0000

    staff type removed

--- src/types/BookAppointmentTypes.ts
+++ src/types/BookAppointmentTypes.ts
@@ -1,14 +1,9 @@
-export type Staff = {
-    id: number;
-    name: string;
-  };
-  
- export type Appointment = {
-    date: string;
-    time: string;
-    firstName: string;
-    lastName: string;
-    emailAddress: string;
-    mobileNumber: string;
-    selectedStaff: string;
-  };
+export type Appointment = {
+  date: string;
+  time: string;
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+  mobileNumber: string;
+  selectedStaff: string;
+};
