1566d931a86d98ff8f0e86aefded14f7434ab9f1
DeeqaJamalini <deeqa.j11@gmail.com>
2024-01-15 10:25:43 +0000

    fixed image variables

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,11 @@
 import "./NavBar.scss";
 import { Link } from 'react-router-dom';
+import brandLogo from '../../assets/images/logo.png';
+import appointmentIcon from '../../assets/images/appointment-icon.png';
+import staffIcon from '../../assets/images/staff-icon.png';
+import clientsIcon from '../../assets/images/clients-icon.png';
+import resourcesIcon from '../../assets/images/resources-icon.png';
+import settingsIcon from '../../assets/images/settings-icon.png';
 
 type NavBarProps = {
     variant: "light" | "dark";}
@@ -10,15 +16,15 @@ const NavBar = ({ variant }: NavBarProps ) => {
 
     return (
         <nav className= {className}>
-            <img alt="Brand Logo" className="nav__logo" src="src/assets/images/logo.png"/>
+            <img alt="Brand Logo" className="nav__logo" src={brandLogo}/>
             <ul className="nav__list">
             <Link to="/home"><li className="nav__list--item"><img src="src/assets/images/home-icon.png" alt="Home Button" className="nav__image"/>Home</li></Link>
-            <Link to="/book-appointment"><li className="nav__list--item"><img src="src/assets/images/appointment-icon.png" alt="Book Appointment" className="nav__image"/>Book Appointment</li></Link>
-            <Link to="/staff"><li className="nav__list--item"><img src="src/assets/images/staff-icon.png" alt="Staff" className="nav__image"/>Staff</li></Link>
-            <Link to="/clients"><li className="nav__list--item"><img src="src/assets/images/clients-icon.png" alt="Clients" className="nav__image"/>Clients</li></Link>
-            <Link to="/resources"><li className="nav__list--item"><img src="src/assets/images/resources-icon.png" alt="Resources" className="nav__image"/>Resources</li></Link>
+            <Link to="/book-appointment"><li className="nav__list--item"><img src={appointmentIcon} alt="Book Appointment" className="nav__image"/>Book Appointment</li></Link>
+            <Link to="/staff"><li className="nav__list--item"><img src={staffIcon} alt="Staff" className="nav__image"/>Staff</li></Link>
+            <Link to="/clients"><li className="nav__list--item"><img src={clientsIcon} alt="Clients" className="nav__image"/>Clients</li></Link>
+            <Link to="/resources"><li className="nav__list--item"><img src={resourcesIcon} alt="Resources" className="nav__image"/>Resources</li></Link>
                 <hr/>
-            <Link to="settings"><li className="nav__list--item"><img src="src/assets/images/settings-icon.png" alt="Settings" className="nav__image"/>Settings</li></Link>
+            <Link to="settings"><li className="nav__list--item"><img src={settingsIcon} alt="Settings" className="nav__image"/>Settings</li></Link>
             </ul>
         </nav>
     )
