f23aa481253715e761939c3c6a072da7e0602ea0
AISimonetta <info@simonettaalejandra.com>
2024-01-15 11:12:39 +0000

    working on icons opacity

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -14,7 +14,7 @@ const NavBar = ({ variant }: NavBarProps ) => {
             <img alt="Brand Logo" className="nav__logo" src="src/assets/images/logo.png"/>
             <ul className="nav__list">
             <Link to="/home"><li className="nav__list--item"><img src={homeIcon} alt="Home Button" className="nav__image nav__image--home"/>Home</li></Link>
-            <Link to="/book-appointment"><li className="nav__list--item nav__list--itemHome"><img src="src/assets/images/appointment-icon.png" alt="Book Appointment" className="nav__image"/>Book Appointment</li></Link>
+            <Link to="/book-appointment"><li className="nav__list--item nav__list--itemHome"><img src="src/assets/images/appointment-icon.png" alt="Book Appointment" className="nav__image nav__image--bookAppointment"/>Book Appointment</li></Link>
             <Link to="/staff"><li className="nav__list--item"><img src="src/assets/images/staff-icon.png" alt="Staff" className="nav__image"/>Staff</li></Link>
             <Link to="/clients"><li className="nav__list--item"><img src="src/assets/images/clients-icon.png" alt="Clients" className="nav__image"/>Clients</li></Link>
             <Link to="/resources"><li className="nav__list--item"><img src="src/assets/images/resources-icon.png" alt="Resources" className="nav__image"/>Resources</li></Link>
