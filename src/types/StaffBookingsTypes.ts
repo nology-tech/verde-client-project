0ffcdc9ac408a1392b19898259db4a18bafe0fe6
J M Adis <jessicaadis7@gmail.com>
2024-01-17 12:00:16 +0000

    updated staffbookingList types + data

--- src/types/StaffBookingsTypes.ts
+++ src/types/StaffBookingsTypes.ts
@@ -1,9 +1,12 @@
 export type StaffBookings = {
     id: number;
     clientName: string;
+    role?: string;
+    dob?: string;
     emailAddress: string;
     mobNumber: string;
     bookingDate: string;
     bookingTime: string;
     path: string;
+    staffMember?: string;
 }
