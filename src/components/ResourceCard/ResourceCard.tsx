d77ac02ce663e6df67b3f1526cd1899305e990fd
Jowita Gogol <jmg88@bath.ac.uk>
2024-01-18 10:04:03 +0000

    Updated styles for resource list

--- src/components/ResourceCard/ResourceCard.tsx
+++ src/components/ResourceCard/ResourceCard.tsx
@@ -15,10 +15,11 @@ const ResourceCard = ({
 }: ResourceCardProps) => {
   return (
     <>
-      
-        <Link to={path} key={id}>
+      <Link to={path} key={id}>
         <div className={`resource-card resource-card--${variant}`}>
-          <p className={`resource-card__name resource-card__name--${variant}`}>{resourceName}</p>
+          <p className={`resource-card__name resource-card__name--${variant}`}>
+            {resourceName}
+          </p>
           <p className="resource-card__quantity">{quantityRemaining}</p>
           <p className="resource-card__cost">{costPerUnit}</p>
           <p className="resource-card__autopurchase">{autoPurchase}</p>
@@ -27,9 +28,8 @@ const ResourceCard = ({
           </p>
 
           <img className="resource-card__arrow" src={arrow} alt="arrow"></img>
-          </div>
-        </Link>
-      
+        </div>
+      </Link>
     </>
   );
 };
