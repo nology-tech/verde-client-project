c075e2a47743fe9f6c7489de9120131e2b39d58d
Matthew <12hughesm@gmail.com>
2024-01-18 15:43:42 +0000

    merged with main

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,12 @@
 import Layout from "../../components/Layout/Layout";
 import NavBar from "../../components/NavBar/NavBar";
 import Header from "../../components/Header/Header";
+import NavCardList from "../../components/NavCardList/NavCardList";
+import { NavCardLabels } from "../../data/NavCardLabels";
 import "./Home.scss";
 import { useNavigate } from "react-router";
 import { logout } from "../../firebase";
-import ItemRequestCard from "../../components/ItemRequestCard/ItemRequestCard";
-import { ResourceCardList1 } from "../../data/ResourceCardLists";
+import Button from "../../components/Button/Button";
 type HomeProps = {
   variant: "light" | "dark";
 
@@ -33,7 +34,8 @@ const Home = ({ variant }: HomeProps) => {
           onClick={handleLogout}
         />
         <div className="main__container">
-       <ItemRequestCard resource={ResourceCardList1[0]} variant={variant} id={0}  placeholder={"Edit ..."}/>
+          <NavCardList labels={NavCardLabels} variant={variant} />
+          <Button size="large" label="Log Out" variant="yellow" path="/" />
         </div>
       </main>
     </Layout>
