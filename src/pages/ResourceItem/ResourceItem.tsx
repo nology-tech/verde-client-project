dde3a78a11e8842095f4aa296095df8ccdd7e34f
Katarzyna Kozlowska <Kat.w.kozlowska@gmail.com>
2024-01-18 16:07:02 +0000

    setup page

---
+++ src/pages/ResourceItem/ResourceItem.tsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout/Layout"
+import "./ResourceItem.scss"
+
+const ResourceItemPage = () => {
+    return (<Layout children={undefined}>
+
+        
+    </Layout>)
+}
+
+export default ResourceItemPage
