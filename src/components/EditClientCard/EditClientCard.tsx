252f4500e4027ba8b3feb273d49a6094295c314b
nrmistry <nilum@outlook.com>
2024-01-19 10:50:43 +0000

    adding in delete modal and cancel button

--- src/components/EditClientCard/EditClientCard.tsx
+++ src/components/EditClientCard/EditClientCard.tsx
@@ -2,8 +2,9 @@ import "./editClientCard.scss";
 import { useState, useEffect } from "react";
 import Button from "../Button/Button";
 import { ClientProfileList } from "../../data/ClientProfileList";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import profilePicture from "../../assets/images/profile-picture.png";
+import Modal from "../Modal/Modal";
 
 type EditClientProps = {
   variant: "dark" | "light";
@@ -20,6 +21,11 @@ const EditClientCard = ({ variant }: EditClientProps) => {
 /*   const [clientPic, setClientPic] = useState<string>(""); */
   const [employed, setEmployed] = useState<string>("");
   const { clientId } = useParams();
+  const [modal, setModal] = useState<boolean>(false);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [saveModal, setSaveModal] = useState<boolean>(false)
+
+  const navigate = useNavigate()
 
   const filteredClient = ClientProfileList.filter(
     (client) => client.id === Number(clientId)
@@ -48,21 +54,45 @@ const EditClientCard = ({ variant }: EditClientProps) => {
       !lastName ||
       !emailAddress ||
       !mobileNumber ||
-      !employed ||
+      // !employed ||
       !dob
     ) {
       console.log("Please fill in all fields");
+      console.log(employer, role, firstName, lastName, emailAddress, mobileNumber, employed, dob)
       return;
     }
   };
 
-  const handleDeleteUser = () => {
-    const index = ClientProfileList.indexOf(client);
-    ClientProfileList.splice(index)
-  };
+  // const handleDeleteUser = () => {
+  //   const index = ClientProfileList.indexOf(client);
+  //   ClientProfileList.splice(index)
+  // };
+
+  const handleDeleteClicked = () => {
+    console.log("confirmed");
+    // const index = ClientProfileList.indexOf(client);
+    // ClientProfileList.splice(index)
+    console.log(ClientProfileList);
+    setModal(!modal);
+  }
+  const handleButtonClicked = () => {
+    setModal(!modal)
+  }
+
+  const handleSaveButton = () => {
+    setSaveModal(!saveModal)
+  }
+
+
+  const modalTwo= [
+    <Button label={"Cancel"} variant={"red-alt"} onClick={handleButtonClicked} />,
+    <Button label={"Confirm"} variant={"red"} onClick={handleDeleteClicked} />,
+  ];
 
   return (
     <div className={`edit-client edit-client--${variant}`}>
+
+      {!modal  && (
       <form className="edit-client__form">
         <div className="leftRight-wrapper">
           <div className="edit-client__form--leftside">
@@ -104,7 +134,7 @@ const EditClientCard = ({ variant }: EditClientProps) => {
             <label>Employed Post Course</label>
                 <select
                   name="employed"
-                  value=""
+                  value={employed}
                   onChange={(e) => setEmployed(e.target.value)}
                 >
                   <option value="YES" >
@@ -145,18 +175,28 @@ const EditClientCard = ({ variant }: EditClientProps) => {
               <Button
                 label="Cancel"
                 variant="purple"
-                onClick={() => console.log("Cancel button clicked")}
+                onClick={() => navigate(-1)}
+            
               />
               </div>
               <div className="submit-button">
-              <Button label="Save" variant="yellow" onClick={handleSubmit} />
+              <Button label="Save" variant="yellow" onClick={handleSaveButton} />
             </div>
           </div>
-          <button className="edit-client__form--rightside--delete" onClick={handleDeleteUser}>Delete User</button>
+          <button className="edit-client__form--rightside--delete" onClick={handleDeleteClicked}>Delete User</button>
         </div>
             </div>
       </form>
-    </div>
+)};
+
+{modal &&(
+  <Modal title={"Client Saved!"} buttons={[<Button label={"Back"} variant={"yellow"} onClick={handleSaveButton} />]} variant={variant}/> 
+)}
+
+{modal &&(
+   <Modal title={"Are you sure you want to delete this client?"} buttons={modalTwo} variant={variant}/> 
+)}
+</div>
   );
 };
 
